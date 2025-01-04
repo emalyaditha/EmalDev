f01ad09f0a74838c44e7149e6497db42f8126385
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2025-01-04 17:46:41 +0530

    New Style

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let isExperienceVisible = false;
 
-    document.addEventListener('DOMContentLoaded', function() {
-      let isExperienceVisible = false;
+  document
+    .getElementById("toggle-button")
+    .addEventListener("click", function () {
+      const experience = document.getElementById("experience");
+      const education = document.getElementById("education");
 
-      document.getElementById('toggle-button').addEventListener('click', function() {
-        const experience = document.getElementById('experience');
-        const education = document.getElementById('education');
+      if (isExperienceVisible) {
+        experience.style.display = "none";
+        education.style.display = "block";
+        this.textContent = "Show Experience"; // Update button text
+      } else {
+        experience.style.display = "block";
+        education.style.display = "none";
+        this.textContent = "Show Education"; // Update button text
+      }
 
-        if (isExperienceVisible) {
-          experience.style.display = 'none';
-          education.style.display = 'block';
-          this.textContent = 'Show Experience'; // Update button text
-        } else {
-          experience.style.display = 'block';
-          education.style.display = 'none';
-          this.textContent = 'Show Education'; // Update button text
-        }
-
-        isExperienceVisible = !isExperienceVisible;
-      });
+      isExperienceVisible = !isExperienceVisible;
     });
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  const timeline = document.getElementById("experience");
+  timeline.classList.remove("hidden");
+});
